5a2d08f5460a4cc03198aa9d5fec9acde9097014
Fabio Sosa <fabio_sosapy@outlook.com>
2024-07-11 23:26:18 -0400

    finished project

---
+++ src/seed/seed.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { SeedService } from './seed.service';
+
+@Controller('seed')
+export class SeedController {
+  constructor(private readonly seedService: SeedService) {}
+
+  @Get()
+  executeSeed() {
+    return this.seedService.executeSeed();
+  }
+}
+
+// estoy en el capitulo 5 - realizar peticion http en minuto 2
