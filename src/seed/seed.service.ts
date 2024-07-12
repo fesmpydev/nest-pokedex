79c2e6d5886e0cd2c8949272c60dd516cd9e5d84
Fabio Sosa <fabio_sosapy@outlook.com>
2024-07-12 18:13:09 -0400

    antes de implementar nueva funcionalidad

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { Seed } from './interfaces/seed.interface';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return 'Seed execute';
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed() {
+    const { data } = await this.axios.get<Seed>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
+    );
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+    });
   }
 }
